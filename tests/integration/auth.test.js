fa71badd2137d58848455a932c08990209a49faa
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2020-10-31 16:22:00 +0530

    token expiry changed

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -389,7 +389,7 @@ describe('Auth middleware', () => {
 
   test('should call next with unauthorized error if the token is not an access token', async () => {
     await insertUsers([userOne]);
-    const expires = moment().add(config.jwt.accessExpirationMinutes, 'minutes');
+    const expires = moment().add(config.jwt.accessExpirationDays, 'days');
     const refreshToken = tokenService.generateToken(userOne._id, expires, tokenTypes.REFRESH);
     const req = httpMocks.createRequest({ headers: { Authorization: `Bearer ${refreshToken}` } });
     const next = jest.fn();
@@ -404,7 +404,7 @@ describe('Auth middleware', () => {
 
   test('should call next with unauthorized error if access token is generated with an invalid secret', async () => {
     await insertUsers([userOne]);
-    const expires = moment().add(config.jwt.accessExpirationMinutes, 'minutes');
+    const expires = moment().add(config.jwt.accessExpirationDays, 'days');
     const accessToken = tokenService.generateToken(userOne._id, expires, tokenTypes.ACCESS, 'invalidSecret');
     const req = httpMocks.createRequest({ headers: { Authorization: `Bearer ${accessToken}` } });
     const next = jest.fn();
