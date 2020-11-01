14213f2d34bf2882e56ffa3b3aacbfdbbd7de3ba
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2020-11-01 22:17:29 +0530

    changes to shitty joi errors

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -12,7 +12,8 @@ const validate = (schema) => (req, res, next) => {
 
   if (error) {
     const errorMessage = error.details.map((details) => details.message).join(', ');
-    return next(new ApiError(httpStatus.BAD_REQUEST, errorMessage));
+    return next();
+    // return next(new ApiError(httpStatus.BAD_REQUEST, errorMessage));
   }
   Object.assign(req, value);
   return next();
