96b3adeac9676e6b4099b75ff074ca19735a983a
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2020-11-12 21:40:58 +0530

    Add api for login with token

--- src/models/token.model.js
+++ src/models/token.model.js
@@ -16,7 +16,7 @@ const tokenSchema = mongoose.Schema(
     },
     type: {
       type: String,
-      enum: [tokenTypes.REFRESH, tokenTypes.RESET_PASSWORD],
+      enum: [tokenTypes.REFRESH, tokenTypes.RESET_PASSWORD, tokenTypes.ACCESS],
       required: true,
     },
     expires: {
